4a9ea4345ff6c9872a1500354accc5d159742c7f
SantiagoBedoya <a.sbedoya200110@gmail.com>
2024-09-23 11:22:31 -0500

    First commit

---
+++ cmd/web/handlers.go
@@ -0,0 +1,40 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+	"text/template"
+)
+
+func (app *application) home(w http.ResponseWriter, r *http.Request) {
+	files := []string{
+		"./ui/html/pages/base.tmpl",
+		"./ui/html/pages/partials/nav.tmpl",
+		"./ui/html/pages/home.tmpl",
+	}
+
+	ts, err := template.ParseFiles(files...)
+	if err != nil {
+		app.serverError(w, err)
+		return
+	}
+
+	err = ts.ExecuteTemplate(w, "base", nil)
+	if err != nil {
+		app.serverError(w, err)
+	}
+}
+
+func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
+	id, err := strconv.Atoi(r.URL.Query().Get("id"))
+	if err != nil || id < 1 {
+		app.notFound(w)
+		return
+	}
+	fmt.Fprintf(w, "Displaying a specific snippet with id %d...", id)
+}
+
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("Create a new snippet..."))
+}
